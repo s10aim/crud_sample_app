d39ce431fcaf1af18ee6f33e7e609afd222b7c80
tanahara <s10.vermouth.4869@gmail.com>
2020-06-07 13:18:23 +0900

    rails  g controller コマンドで作成されるファイルを制限

--- db/schema.rb
+++ db/schema.rb
@@ -21,5 +21,4 @@ ActiveRecord::Schema.define(version: 2020_06_07_041008) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
